a08825fd170cc2995cd8fcd6918be35e37ee7a4e
agenceimsea <agence.imsea@gmail.com>
2026-05-18 01:24:43 +0200

    Update gtm-config.js

--- gtm-config.js
+++ gtm-config.js
@@ -12,4 +12,4 @@ window.dataLayer = window.dataLayer || [];
 // 2. ID du conteneur GTM
 //    Remplace "GTM-XXXXXX" par ton propre ID
 //    (Récupéré sur https://tagmanager.google.com)
-window.GTM_ID = "GTM-XXXXXX";
+window.GTM_ID = "GTM-NQZ7HDTM";
